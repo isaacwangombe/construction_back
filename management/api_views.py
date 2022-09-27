9b3e62da07d37d84288b1de56efa7eba182790cb
isaac mariga <isaacwangombe5@gmail.com>
2022-09-27 13:52:50 +0300

    adding readme

--- management/api_views.py
+++ management/api_views.py
@@ -66,6 +66,51 @@ class ItemList(APIView):
 
 
 
+class BySupplierList(APIView):
+  def get(self, request,supplier, format=None):
+    items = Item.filter_by_supplier(supplier)
+    serializers = ItemSerializer(items, many=True)
+    return Response(serializers.data)
+
+  def post(self, request, format=None):
+    serializers = ItemSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ByDateList(APIView):
+  def get(self, request, date, format=None):
+    items = Item.filter_by_date(date)
+    serializers = ItemSerializer(items, many=True)
+    return Response(serializers.data)
+
+  def post(self, request, format=None):
+    serializers = ItemSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ById(APIView):
+  def get(self, request,id, format=None):
+    item = Item.get_by_id(id)
+    serializers = ItemSerializer(item, many=False)
+    return Response(serializers.data)
+
+  def post(self, request, format=None):
+    serializers = ItemSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 class ItemList(APIView):
   def get(self, request, format=None):
     all_item = Item.get_all()
--- management/urls.py
+++ management/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
   path('', views.welcome,name = 'test'),
 
 
-  path('api/date', api_views.DateList.as_view()),
-  path('api/units', api_views.UnitsList.as_view()),
-  path('api/supplier', api_views.SupplierList.as_view()),
-  path('api/item', api_views.ItemList.as_view()),
+  path('api/all_date', api_views.DateList.as_view()),
+  path('api/all_units', api_views.UnitsList.as_view()),
+  path('api/all_suppliers', api_views.SupplierList.as_view()),
+  path('api/all_items', api_views.ItemList.as_view()),
+  path('api/item_by_date/<date>', api_views.ByDateList.as_view()),
+  path('api/item_by_supplier/<supplier>', api_views.BySupplierList.as_view()),
+  path('api/item_by_id/<id>', api_views.ById.as_view()),
 ]
