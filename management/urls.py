911fe5ad1de4904f218f0504bbcaa21f3a6f547b
isaac mariga <isaacwangombe5@gmail.com>
2022-09-27 15:43:13 +0300

    deployment

--- management/urls.py
+++ management/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
   path('api/all_units', api_views.UnitsList.as_view()),
   path('api/all_suppliers', api_views.SupplierList.as_view()),
   path('api/all_items', api_views.ItemList.as_view()),
-  path('api/item_by_date/<date>', api_views.ByDateList.as_view()),
+  path('api/item_by_dates/<date>', api_views.ByDateList.as_view()),
   path('api/item_by_supplier/<supplier>', api_views.BySupplierList.as_view()),
   path('api/item_by_id/<id>', api_views.ById.as_view()),
 ]
