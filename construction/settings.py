2675659d2c72fdcdeafcfe544e6a4968c9d43e8c
isaac mariga <isaacwangombe5@gmail.com>
2022-11-01 09:54:03 +0300

    deployment

--- construction/settings.py
+++ construction/settings.py
@@ -185,10 +185,11 @@ THOUSAND_SEPARATOR = '\xa0'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, "static"),
-]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -206,7 +207,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-CSRF_TRUSTED_ORIGINS = ['https://angelic-art-gallery-production.up.railway.app','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://construction.up.railway.app/','https://*.127.0.0.1']
 
 
 DATABASES = {
