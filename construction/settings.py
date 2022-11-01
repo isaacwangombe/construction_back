af87a4f1e0b5cfa66617403c2dcd7c61cf4c5b71
isaac mariga <isaacwangombe5@gmail.com>
2022-11-01 10:02:55 +0300

    deployment

--- construction/settings.py
+++ construction/settings.py
@@ -185,7 +185,7 @@ THOUSAND_SEPARATOR = '\xa0'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
